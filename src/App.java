db47b8dc3917ad20f0e2121c9583cb6bfc2be44c
WyattDCake <wyattcake1@gmail.com>
2023-10-12 17:30:24 -0400

    Not doing Swing, doing JavaFX. New start

--- src/App.java
+++ src/App.java
@@ -1,10 +1,30 @@
+import java.io.IOException;
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        UserInterface UI = new UserInterface(scanner);
-        UI.start();
-        new GUI();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root;
+        try{
+        root = FXMLLoader.load(getClass().getResource("schedule.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args){
+        launch(args);
+        //Scanner scanner = new Scanner(System.in);
+        //UserInterface UI = new UserInterface(scanner);
+        //UI.start();
     }
 }
