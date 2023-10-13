deb83ef16b8ffe8bf020a3819e9208d42051ef5d
WyattDCake <wyattcake1@gmail.com>
2023-10-13 19:10:22 -0400

    Stylzing changes and did other shit

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Idea: input homework due dates & job dates, retrieve schedule for next dates
     -add new dates
     -mark completed
     
+## Scene Dimensions:
+each scene is 1280x720
+corner buttons are 150x50
+large buttons are 400x40
--- src/scenes/SceneController.java
+++ src/scenes/SceneController.java
@@ -1,9 +1,14 @@
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -18,15 +23,37 @@ public class SceneController {
     String css = this.getClass().getResource("sceneDesign.css").toExternalForm();
     InputOutput inputOutput;
     ArrayList<Event> schedule;
+    private Image mainImage = new Image("main.png");
+    /* all from failed Slider attempt
+    @FXML 
+    private Slider startTimeSlider = new Slider();
+    @FXML
+    private Slider endTimeSlider = new Slider();
+    @FXML 
+    private Label startLabel = new Label();
+    @FXML
+    private Label endLabel = new Label();
+    */
     @FXML
     private Text displayText = new Text();
+    @FXML
+    private ImageView mainView = new ImageView();
 
     @FXML
     public void initialize(){
+        //input for schedule
         inputOutput = new InputOutput();
         schedule = inputOutput.inputSchedule();
         Collections.sort(schedule);
+        //display schedule needs to be here
         displaySchedule();
+        //set mainView = mainImage
+        mainView.setImage(mainImage);
+        /*startTimeSlider settings
+        startTimeSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+        startLabel.setText("Shift Starts at: " + getTime(newValue.intValue()));
+        });
+        */
     }
   
     public void switchToMainScene(ActionEvent event) throws IOException{
@@ -90,6 +117,40 @@ public class SceneController {
         }
         displayText.setText(text.toString());
     }
-
-
+    public void addEditButtons(){
+        
+    }
+    /* failed slider experiement
+    public String getTime(int time){
+        String stringAMPM = " AM";
+        int hour = 0;
+        String min;
+        while(time > 4){
+            time-=4;
+            hour++;
+        }
+        if(hour > 12){
+            stringAMPM =" PM";
+            hour-=12;
+        }
+        switch(time){
+            case 0:
+                min = ":00";
+                break;
+            case 1:
+                min = ":15";
+                break;
+            case 2:
+                min = ":30";
+                break;
+            case 3:
+                min = ":45";
+                break;
+            default:
+                min = "error";
+                break;
+        }
+        return (String.valueOf(hour)+min+stringAMPM);
+    }
+    */
 }
