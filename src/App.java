86acc6f7d9cd1aa23d9ff4d9d78f2901d45cb568
WyattDCake <wyattcake1@gmail.com>
2023-10-13 01:31:56 -0400

    Learned a bit more JavaGX,proud of myself

--- src/App.java
+++ src/App.java
@@ -3,23 +3,28 @@ import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root;
         try{
-        root = FXMLLoader.load(getClass().getResource("schedule.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        } catch (IOException e){
+            Parent root = FXMLLoader.load(getClass().getResource("mainframe.fxml"));
+            Scene scene = new Scene(root,400,400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(IOException e){
             e.printStackTrace();
         }
+
     }
     public static void main(String[] args){
         launch(args);
